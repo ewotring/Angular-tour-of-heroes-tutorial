d82714752ee567b588b0d11fef8878625e92ce00
Erik Wotring <ewotring@gmail.com>
2018-08-17 21:17:34 -0500

    Build routable HeroDetail component

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -24,7 +24,10 @@ export class HeroDetailComponent implements OnInit {
 
   getHero(): void {
     const id = +this.route.snapshot.paramMap.get('id');
-    this.heroService.getHeroes(id).subscribe(hero => this.hero = hero);
+    this.heroService.getHero(id).subscribe(hero => this.hero = hero);
   }
 
+  goBack(): void {
+    this.location.back();
+  }
 }
